682e29a216560d45519a9286f6ed8814008601d3
Nguyen Danh Nam <namnd2802@gmail.com>
2020-04-21 02:28:23 +0700

    fd

--- app.py
+++ app.py
@@ -51,6 +51,6 @@ def api():
 if __name__ == "__main__":
     models = Classifier()
     models.load_model("model/colabs/model_v1")
-    with open("model/colabs/transform") as f:
+    with open("model/colabs/transform", "rb") as f:
         transform = pickle.load(f)
     app.run()
